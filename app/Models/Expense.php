f6c35e05fb58ba1e3fca70d6e4c4ad1752c4dd7c
oussama-achaoui <kunoussa@gmail.com>
2024-01-24 15:24:54 +0100

    incomes&expenses tables added

---
+++ app/Models/Expense.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'amount', 'transaction_date', 'payment_method'];
+
+}
