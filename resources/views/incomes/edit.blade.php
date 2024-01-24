580620e707f3a2a95a0749b6de8935c666baced9
oussama-achaoui <kunoussa@gmail.com>
2024-01-24 16:01:18 +0100

    Incomes Section

--- resources/views/incomes/edit.blade.php
+++ resources/views/incomes/edit.blade.php
@@ -1,77 +1,68 @@
+<!-- resources/views/incomes/edit.blade.php -->
+
 @extends('layouts.admin')
 
 @section('content')
+
     <!-- Begin Page Content -->
     <div class="container-fluid">
 
         <!-- Page Heading -->
-        <h1 class="h3 mb-2 text-gray-800">{{ clean(trans('Edit Business Card'), ['Attr.EnableID' => true]) }}</h1>
+        <h1 class="h3 mb-2 text-gray-800">Edit Income</h1>
 
         <!-- DataTales Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">{!! trans('Edit Business Card') !!}</h6>
+                <h6 class="m-0 font-weight-bold text-primary">Edit Income</h6>
             </div>
             <div class="card-body">
 
-                <a href="{{ route('business-cards.index') }}"
-                    class="btn btn-primary btn-back">{{ clean(trans('Back to Cartes de visite'), ['Attr.EnableID' => true]) }}</a>
+                <a href="{{ route('incomes.index') }}" class="btn btn-primary btn-back">Back to Incomes</a>
 
                 @include('includes.form-errors')
 
                 <div class="row">
                     <div class="col-md-12">
 
-                        <form action="{{ route('business-cards.update', $businessCard->id) }}" method="POST">
+                        <form action="{{ route('incomes.update', $income->id) }}" method="POST">
                             @csrf
                             @method('PUT')
 
                             <div class="row">
-                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="col-xs-12 col-sm-6 col-md-6">
                                     <div class="form-group">
-                                        <label
-                                            for="name">{{ clean(trans('niva-backend.name'), ['Attr.EnableID' => true]) }}</label>
-                                        <input type="text" name="name" class="form-control"
-                                            value="{{ $businessCard->name }}" required>
+                                        <strong>Name:</strong>
+                                        <input type="text" name="name" class="form-control" value="{{ $income->name }}" required>
                                     </div>
                                 </div>
-                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="col-xs-12 col-sm-6 col-md-6">
                                     <div class="form-group">
-                                        <label
-                                            for="role">{{ clean(trans('Role'), ['Attr.EnableID' => true]) }}</label>
-                                        <input type="text" name="role" class="form-control"
-                                            value="{{ $businessCard->role }}" required>
+                                        <strong>Description:</strong>
+                                        <input type="text" name="description" class="form-control" value="{{ $income->description }}" required>
                                     </div>
                                 </div>
-                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="col-xs-12 col-sm-6 col-md-6">
                                     <div class="form-group">
-                                        <label
-                                            for="phone">{{ clean(trans('niva-backend.phone'), ['Attr.EnableID' => true]) }}</label>
-                                        <input type="text" name="phone" class="form-control"
-                                            value="{{ $businessCard->phone }}" required>
+                                        <strong>Amount:</strong>
+                                        <input type="number" name="amount" class="form-control" value="{{ $income->amount }}" required>
                                     </div>
                                 </div>
-                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="col-xs-12 col-sm-6 col-md-6">
                                     <div class="form-group">
-                                        <label
-                                            for="email">{{ clean(trans('niva-backend.email'), ['Attr.EnableID' => true]) }}</label>
-                                        <input type="email" name="email" class="form-control"
-                                            value="{{ $businessCard->email }}" required>
+                                        <strong>Transaction Date:</strong>
+                                        <input type="date" name="transaction_date" class="form-control" value="{{ $income->transaction_date }}" required>
                                     </div>
                                 </div>
-                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="col-xs-12 col-sm-6 col-md-6">
                                     <div class="form-group">
-                                        <label
-                                            for="address">{{ clean(trans('niva-backend.address'), ['Attr.EnableID' => true]) }}</label>
-                                        <input type="address" name="address" class="form-control"
-                                            value="{{ $businessCard->address }}" required>
+                                        <strong>Payment Method:</strong>
+                                        <input type="text" name="payment_method" class="form-control" value="{{ $income->payment_method }}" required>
                                     </div>
                                 </div>
                                 <!-- Add more fields as needed -->
 
                                 <div class="col-xs-12 col-sm-12 col-md-12 text-right">
-                                    <button type="submit"
-                                        class="btn btn-primary">{{ clean(trans('Update Business Card'), ['Attr.EnableID' => true]) }}</button>
+                                    <button type="submit" class="btn btn-primary">Update Income</button>
                                 </div>
                             </div>
                         </form>
